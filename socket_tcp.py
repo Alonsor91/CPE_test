7e743ee29566b9d0a56f302151c877157c5dd159
Frantsuzov <frantsuzov.v.a@gmail.com>
2018-10-23 12:36:23 +0300

    add tcp

---
+++ socket_tcp.py
@@ -0,0 +1,10 @@
+import socket
+
+s_tcp = int(input("Start UDP port:")) #Start TCP dst port
+e_tcp = int(input("End UDP port:")) #End TCP dst port
+d_ip = input("Destenation IP:") #Destination IP address
+
+for tcp in range(s_tcp,e_tcp):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect_ex((d_ip, tcp))
+    print ("TCP, Dst Port: {} IPv4, Dst: {}".format(tcp,d_ip))
